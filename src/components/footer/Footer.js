4a84b4ec10e0e133dd084a29e4e2414f12e11563
unknown <pamporova.diana@gmail.com>
2020-12-07 15:30:31 +0200

    footer copyright align change

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ import Social from '../social/Social.js';
 function Footer() {
     return (
         <footer>
-            <h5>Copyright</h5>
+            <h5>Copyright 2019-2021</h5>
             <Social />
         </footer>
     );
